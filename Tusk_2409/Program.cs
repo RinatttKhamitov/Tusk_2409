b026aec17fdf8ad1c72e93e014e2a464827ccbda
Ринат Хамитов <stoneaz@mail.ru>
2022-09-29 18:30:17 +0300

    Добавьте файлы проекта.

---
+++ Tusk_2409/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tusk_2409
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введи высоту форточки a = ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Введи ширину форточки b = ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("Введи диаметр головы d = ");
+            int d = int.Parse(Console.ReadLine());
+            if (d > a + 1 & d > b + 1)
+            {
+                Console.WriteLine("");
+            }
+        }
+    }
+}
